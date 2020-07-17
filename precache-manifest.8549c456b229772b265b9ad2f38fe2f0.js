95c926b38a289f9200c030c447e64fd6dcdf21df
Salih Ozyurt <salih.zyurt@gmail.com>
2020-07-17 04:20:02 +0300

    add react files

---
+++ precache-manifest.8549c456b229772b265b9ad2f38fe2f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93daeb6b111f1457ddaf9649fac264f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4837b642c7ce5abc27bd",
+    "url": "/static/css/main.0ece5576.chunk.css"
+  },
+  {
+    "revision": "14127dc31df663e9ed76",
+    "url": "/static/js/2.b1c94fe9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1c94fe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4837b642c7ce5abc27bd",
+    "url": "/static/js/main.d0dd2158.chunk.js"
+  },
+  {
+    "revision": "d7b46aa80595a1311dc0",
+    "url": "/static/js/runtime-main.245afb79.js"
+  },
+  {
+    "revision": "3f4cec540a7550630ab2e7d6c43523e8",
+    "url": "/static/media/HeaderLogo.3f4cec54.png"
+  }
+]);
